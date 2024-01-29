032278536f9e5010c5044ed2231e5cc66808c475
shusakai <shunsukes510@gmail.com>
2024-01-29 21:47:43 +0900

    20240129

---
+++ build/lib/skny/pl.py
@@ -0,0 +1,6 @@
+"""Main module."""
+from .plotting.clustering_heatmap import clustering_heatmap
+from .plotting.deg_manhattan_plot import deg_manhattan_plot
+from .plotting.distance_gene_plot import distance_gene_plot
+from .plotting.equidistant_curves import equidistant_curves
+from .plotting.preprocessed_images import preprocessed_images
