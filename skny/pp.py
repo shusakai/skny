c327f8af17ff8b904f3819586f9792d6e1705c24
shusakai <shunsukes510@gmail.com>
2024-02-02 14:45:04 +0900

    20240202

--- skny/pp.py
+++ skny/pp.py
@@ -1,4 +1,5 @@
 """Main module."""
 from .preprocessing.distance_calculator import calculate_distance
 from .preprocessing.convert_indivisual_peri import convert_indivisual_peri
+from .preprocessing.convert_indivisual_solid import convert_indivisual_solid
 
